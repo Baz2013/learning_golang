a51ffe35f3607a1345380e78339db23858d46288
gucb3 <chuan.gu6@gmail.com>
2022-05-11 10:39:17 +0800

    添加表格驱动测试用例

--- unit_test_demo/split_test.go
+++ unit_test_demo/split_test.go
@@ -8,6 +8,9 @@ import (
 // 在目标目录下执行单元测试命令
 // 命令: go test
 // go test -v # 输出详细信息
+// go test -run=xx -v # -run 参数可以正则匹配测试方法名
+// 例如： go test -run=Sep -v # 在这里可以匹配到TestSplitWithComplexSep()方法
+
 func TestSplit(t *testing.T) { // 测试函数名必须以Test开头，必须接收一个*testing.T类型参数
 	got := Split("a:b:c", ":")         // 程序输出的结果
 	want := []string{"a", "b", "c"}    // 期望的结果
@@ -21,5 +24,32 @@ func TestSplitWithComplexSep(t *testing.T) {
 	want := []string{"a", "d"}
 	if !reflect.DeepEqual(want, got) {
 		t.Errorf("expected:%v, got:%v", want, got)
+	}
+}
+
+// 表格驱动测试
+func TestSplitAll(t *testing.T) {
+	// 定义测试表格
+	// 这里使用匿名结构体定义了若干个测试用例
+	// 并且为每个测试用例设置了一个名称
+	tests := []struct {
+		name  string
+		input string
+		sep   string
+		want  []string
+	}{
+		{"base case", "a:b:c", ":", []string{"a", "b", "c"}},
+		{"wrong sep", "a:b:c", ",", []string{"a:b:c"}},
+		{"more sep", "abcd", "bc", []string{"a", "d"}},
+		{"leading sep", "沙河有沙又有河", "沙", []string{"", "河有", "又有河"}},
+	}
+	// 遍历测试用例
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) { // 使用t.Run()执行子测试
+			got := Split(tt.input, tt.sep)
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("expected:%#v, got:%#v", tt.want, got)
+			}
+		})
 	}
 }
